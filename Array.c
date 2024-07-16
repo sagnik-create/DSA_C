820350e39d7067889cb537ffe8e05e6610397b5b
Sagnik Dey <146818206+sagnik-create@users.noreply.github.com>
2024-07-16 06:56:05 +0000

    Day 1

--- Array.c
+++ Array.c
@@ -7,10 +7,20 @@ int arrcr(int n){
   }
   return arr[n];
 }
-
+int arrch(int useindex; int val){
+    int value;
+    value = useindex-1
+    if (value == 0){
+        return 0
+    }
+    arr[value] = val
+}
 int main(){
-  int n;
+  int n,pos,ch;
   printf("Enter the size of Array");
   scanf("%d",&n);
   arrcr(n);
+  printf("Enter the postion")
+  scanf("")
+  arrch(pos,ch);
 }
