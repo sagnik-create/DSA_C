8423bfccce70327a4581d436b036858d4e81a720
Sagnik Dey <146818206+sagnik-create@users.noreply.github.com>
2024-08-05 19:06:03 +0000

    Binary and linear Search

--- search.c
+++ search.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
-// Function to perform binary search
 int binarySearch(int arr[], int size, int target) {
     int left = 0, right = size - 1;
     while (left <= right) {
         int mid = left + (right - left) / 2;
-
-        // Check if target is present at mid
         if (arr[mid] == target)
             return mid;
-
-        // If target is greater, ignore left half
         if (arr[mid] < target)
             left = mid + 1;
-
-        // If target is smaller, ignore right half
         else
             right = mid - 1;
     }
-
-    // If target is not present in array
     return -1;
 }
 
@@ -30,8 +21,9 @@ int linearSearch(int arr[], int size, int target) {
     }
     return -1;  
 }
+
 int main() {
-    int size,target;
+    int size, target;
     printf("Enter the number of elements in the array: ");
     scanf("%d", &size);
 
@@ -44,18 +36,19 @@ int main() {
 
     printf("Enter the target value to search: ");
     scanf("%d", &target);
+
     int result1 = binarySearch(arr, size, target);
     int result2 = linearSearch(arr, size, target);
     
     if (result1 != -1)
-        printf("Element is present at index %d\n through Binary Search", result1);
+        printf("Element is present at index %d through Binary Search\n", result1);
     else
         printf("Element is not present in array through Binary Search\n");
+    
     if (result2 != -1)
-        printf("Element is present at index %d\n through Linear Search", result2);
+        printf("Element is present at index %d through Linear Search\n", result2);
     else
         printf("Element is not present in array through Linear Search\n");
-
 
     return 0;
 }
