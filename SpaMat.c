fc9bf586482c9964c155c07a79506cb45a2b4619
Sagnik Dey <146818206+sagnik-create@users.noreply.github.com>
2024-08-07 08:48:47 +0000

    Sparse Matrix code

--- SpaMat.c
+++ SpaMat.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
 
 int main(){
-    int arr[3][3] = {{0,0,1},{0,3,0},{2,0,0}};
-    int count=0;
-    int i;
-    int j;
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            if(arr[i][j] != 0 ){
+    int arr[4][4] = {{0,1,0,0}, {2,0,0,2}, {0,3,0,5}, {0,0,0,6}};
+    int count = 0;
+    int i, j;
+
+    // Counting non-zero elements
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            if(arr[i][j] != 0){
                 count++;
             }
         }
     }
-    for(int k=0;k<3;k++){
-        for(int l=0; l<count; l++){
-            arr[1][count] = i;
-            arr[2][count] = j;
-            arr[3][count] = arr[i][j];
+
+    // Declare sparse array with the correct size
+    int sparse[count][3];
+    int k = 0; // Counter for sparse array index
+
+    // Storing non-zero elements in sparse representation
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            if(arr[i][j] != 0){
+                sparse[k][0] = i;
+                sparse[k][1] = j;
+                sparse[k][2] = arr[i][j];
+                k++;
+            }
         }
     }
-    for(int k=0;k<3;k++){
-        for(int l=0; l<count; l++){
-            printf("%d ",arr[k][l]);
-        }
+
+    // Printing the sparse matrix representation
+    printf("Row Index\tColumn Index\tValue\n");
+    for(i = 0; i < count; i++){
+        printf("%d\t\t%d\t\t%d\n", sparse[i][0], sparse[i][1], sparse[i][2]);
     }
+
+    return 0;
 }
