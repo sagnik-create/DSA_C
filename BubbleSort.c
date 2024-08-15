87b59a42a13f484f9f43ff76ade9b00a78c56f29
Sagnik Dey <146818206+sagnik-create@users.noreply.github.com>
2024-08-15 08:40:20 +0000

    Edit and Insertion

--- BubbleSort.c
+++ BubbleSort.c
@@ -23,35 +23,20 @@ void Bubsort(int n, int arr[]) {
 }
 
 int main() {
-    // srand(time(NULL));  // Seed the random number generator
-
-    // while (1) {
         int n;
         printf("Enter The Number");
         scanf("%d",&n);
-        // = rand() % 10 + 2;  // Generate n between 2 and 11
         int arr[n];
 
         printf("Create an array: ");
         for (int i = 0; i < n; i++) {
             scanf("%d",&arr[i]);
-            // arr[i] = rand() % 1000;  // Generate array elements between 0 and 999
-            // printf("%d ", arr[i]);
         }
         printf("\n");
-
-        // clock_t start = clock();  // Start the clock
 
         printf("Sorted array: ");
         Bubsort(n, arr);
 
-        // clock_t end = clock();  // End the clock
-        // double time_used = ((double) (end - start)) / CLOCKS_PER_SEC;  // Calculate the time taken
-        // printf("Time taken to sort the array: %f seconds\n", time_used);
-
-        // Add a delay to avoid flooding the console
-        // sleep(1);  // Sleep for 1 second
-    // }
-
     return 0;
 }
+
--- SelectionSort.c
+++ SelectionSort.c
@@ -11,7 +11,7 @@ void SelSort(int arr[],int size){
     for (i=0 ; i<size-1; i++){
         min = i;
     }
-    for (j=0; j<size; j++){
+    for (j=0; j<size ; j++){
         if(arr[j] <= arr[min]){
             min = j;
         }
